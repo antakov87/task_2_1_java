6e7f078d8278b826e3cee553c84a1bd8bb2c464e
Антаков Сергей <antakov87@mail.ru>
2022-07-03 13:31:33 +0400

    edit file

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticket_price = 59;
-        int y = 20;
-        int bonus = ticket_price / y;
+        int ticketPrice = 59;
+        int oneMile = 20;
+        int bonus = ticketPrice / oneMile;
         System.out.println(bonus);
 
     }

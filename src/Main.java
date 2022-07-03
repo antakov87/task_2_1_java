9736a0199bb09f988d8e675272fd6cdb7a237b0f
Антаков Сергей <antakov87@mail.ru>
2022-07-03 12:15:35 +0400

    creating task

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket_price = 59;
+        int y = 20;
+        int bonus = ticket_price / y;
+        System.out.println(bonus);
+
+    }
+}
